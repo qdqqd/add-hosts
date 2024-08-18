79308c2eea0405d61ea33e05e7ca068ad00a3f17
by柯乐 <1010456736@qq.com>
2024-08-18 17:03:18 +0800

    Add files via upload

---
+++ addhosts.js
@@ -0,0 +1,30 @@
+const fs = require('fs');
+const axios = require('axios');
+
+const addhostsUrls = [
+  'https://raw.githubusercontent.com/lingeringsound/10007_auto/master/all',
+  'https://raw.githubusercontent.com/rentianyu/Ad-set-hosts/master/hosts',
+  'https://pgl.yoyo.org/adservers/serverlist.php?hostformat=hosts&showintro=0&mimetype=plaintext',
+  'https://gitlab.com/ineo6/hosts/-/raw/master/next-hosts',
+  'https://raw.hellogithub.com/hosts',
+  'https://raw.githubusercontent.com/maxiaof/github-hosts/master/hosts'
+];
+
+// 函数来获取数据并写入文件
+async function fetchAndUpdateFile(urls, filePath) {
+  try {
+    let allData = '';
+    for (const url of urls) {
+      const response = await axios.get(url);
+      allData += response.data + '\n'; // 将每个响应数据添加到最终数据中
+    }
+    fs.writeFileSync(filePath, allData); // 写入文件
+    console.log(`Updated ${filePath} successfully.`);
+  } catch (error) {
+    console.error(`Error updating ${filePath}:`, error);
+  }
+}
+
+
+fetchAndUpdateFile(addhostsUrls, 'addhosts');
+
