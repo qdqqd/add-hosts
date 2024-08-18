05ac9773bf6f81ad42ec5d4d5dcdb1e726702413
by柯乐 <1010456736@qq.com>
2024-08-18 18:28:08 +0800

    Update addhosts.js

--- addhosts.js
+++ addhosts.js
@@ -1,6 +1,7 @@
 const fs = require('fs');
 const axios = require('axios');
 
+// 定义要获取的链接
 const addhostsUrls = [
   'https://raw.githubusercontent.com/lingeringsound/10007_auto/master/all',
   'https://raw.githubusercontent.com/rentianyu/Ad-set-hosts/master/hosts',
@@ -18,13 +19,35 @@ async function fetchAndUpdateFile(urls, filePath) {
       const response = await axios.get(url);
       allData += response.data + '\n'; // 将每个响应数据添加到最终数据中
     }
-    fs.writeFileSync(filePath, allData); // 写入文件
+
+    // 处理数据
+    let processedData = processTextData(allData);
+
+    // 添加更新时间和作者信息
+    const now = new Date();
+    const header = `# Updated on: ${now.toISOString()}\n# Author: by 柯乐\n# Homepage: https://www.qdqqd.com\n# https://raw.githubusercontent.com/qdqqd/add-hosts/main/addhosts\n# 用于去除各种广告\n\n`;
+    processedData = header + processedData;
+
+    // 写入处理后的数据
+    fs.writeFileSync(filePath, processedData);
     console.log(`Updated ${filePath} successfully.`);
   } catch (error) {
     console.error(`Error updating ${filePath}:`, error);
   }
 }
 
+// 函数来处理文本数据
+function processTextData(text) {
+  // 删除以 # 开头的行
+  let lines = text.split('\n').filter(line => !line.trim().startsWith('#'));
+
+  // 将所有的长空格替换成一个空格
+  lines = lines.map(line => line.replace(/\s+/g, ' '));
+
+  // 删除重复的文本行
+  let uniqueLines = Array.from(new Set(lines));
+
+  return uniqueLines.join('\n');
+}
 
 fetchAndUpdateFile(addhostsUrls, 'addhosts');
-
