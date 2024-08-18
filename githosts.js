ab3ef8fbfdf05e03467fa490ca5aab29c83ebf71
by柯乐 <1010456736@qq.com>
2024-08-18 18:18:01 +0800

    Update githosts.js

--- githosts.js
+++ githosts.js
@@ -8,7 +8,6 @@ const githostsUrls = [
   'https://raw.hellogithub.com/hosts'
 ];
 
-
 // 函数来获取数据并写入文件
 async function fetchAndUpdateFile(urls, filePath) {
   try {
@@ -17,13 +16,30 @@ async function fetchAndUpdateFile(urls, filePath) {
       const response = await axios.get(url);
       allData += response.data + '\n'; // 将每个响应数据添加到最终数据中
     }
-    fs.writeFileSync(filePath, allData); // 写入文件
+
+    // 处理数据
+    let processedData = processTextData(allData);
+
+    // 写入处理后的数据
+    fs.writeFileSync(filePath, processedData);
     console.log(`Updated ${filePath} successfully.`);
   } catch (error) {
     console.error(`Error updating ${filePath}:`, error);
   }
 }
 
+// 函数来处理文本数据
+function processTextData(text) {
+  // 删除以 # 开头的行
+  let lines = text.split('\n').filter(line => !line.trim().startsWith('#'));
+
+  // 将所有的长空格替换成一个空格
+  lines = lines.map(line => line.replace(/\s+/g, ' '));
+
+  // 删除重复的文本行
+  let uniqueLines = Array.from(new Set(lines));
+
+  return uniqueLines.join('\n');
+}
 
 fetchAndUpdateFile(githostsUrls, 'githosts');
-
